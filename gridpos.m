71f1bd8037a86bda887ba0d6dd1854fb45a18daa
Johannes Postler <johannes.postler@uibk.ac.at>
2014-02-04 14:53:25 +0100

    Show current filename in main window

--- gridpos.m
+++ gridpos.m
@@ -1,6 +1,11 @@
 function out = gridpos(nlines,nrows,celllinesmin,celllinesmax,cellrowsmin,cellrowsmax,xdist,ydist)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%gridpos Positions uicontrol elements on a grid
+%   I have no fucking clue how this works. Copy another UI element and start
+%   messing around with the values until it is where you want it.
+%   Basically you define a (completely virtual) grid with the first two values
+%   and then align it relative to this grid with the next 4 values.
+%   Keep in mind that xdist/ydist spacing parameters are absolute values in
+%   weird matlab units.
 
 dl=1/nlines;
 dr=1/nrows;
