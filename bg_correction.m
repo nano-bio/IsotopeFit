f9305e4af44bd35b85a58b8f3e186a5ea4cf3eb6
Johannes Postler <johannes.postler@uibk.ac.at>
2014-04-16 16:55:38 +0200

    Use new dataviewer in bg_correction.m

--- bg_correction.m
+++ bg_correction.m
@@ -4,8 +4,8 @@ function [bgcorrectionout, startind, endind] = bg_correction(peakdata,bgcorrecti
 %read out screen size
 scrsz = get(0,'ScreenSize'); 
 
-layoutlines=11;
-layoutrows=5;
+layoutlines=30;
+layoutrows=30;
 
 Parent = figure( ...
     'MenuBar', 'none', ...
@@ -44,138 +44,77 @@ uicontrol(Parent,'Style','Text',...
     'Tag','TextStartMass',...
     'String','Start mass',...
     'Units','normalized',...
-    'Position',gridpos(layoutlines,layoutrows,11,11,1,1,0.01,0.03));
+    'Position',gridpos(layoutlines,layoutrows,29,30,3,4,0.01,0.01));
 
 e_startmass=uicontrol(Parent,'Style','edit',...
     'Tag','edit_startmass',...
     'Units','normalized',...,...
     'String','0',...
     'Background','white',...
-    'Position',gridpos(layoutlines,layoutrows,11,11,2,2,0.05,0.025));
+    'Position',gridpos(layoutlines,layoutrows,29,30,5,6,0.01,0.01));
 
 uicontrol(Parent,'Style','Text',...
     'Tag','TextEndMass',...
     'String','End Mass',...
     'Units','normalized',...
-    'Position',gridpos(layoutlines,layoutrows,11,11,4,4,0.01,0.03));
+    'Position',gridpos(layoutlines,layoutrows,29,30,7,8,0.01,0.01));
 
 e_endmass=uicontrol(Parent,'Style','edit',...
     'Tag','edit_endmass',...
     'Units','normalized',...,...
     'String','End',...
     'Background','white',...
-    'Position',gridpos(layoutlines,layoutrows,11,11,5,5,0.05,0.025));
-
-axis1 = axes('Parent',Parent,...
-             'ActivePositionProperty','OuterPosition',...
-             'Units','normalized',...
-             'Position',gridpos(66,60,17,61,4,60,0.04,0.04));
+    'Position',gridpos(layoutlines,layoutrows,29,30,9,10,0.01,0.01));
+% 
+% axis1 = axes('Parent',Parent,...
+%              'ActivePositionProperty','OuterPosition',...
+%              'Units','normalized',...
+%              'Position',gridpos(66,60,17,61,4,60,0.04,0.04));
+         
+% for backwards compatibility with the existing code, we map updateslider
+% to the function inside the dataviewer object
+dvhandle = dataviewer(Parent, gridpos(layoutlines,layoutrows,4,28,1,layoutrows,0.025,0.01), 50, 40, false);
+axis1 = dvhandle.axes;
+updateslider = dvhandle.updateslider;
 
 e_ndiv=uicontrol(Parent,'Style','edit',...
     'Tag','edit_ndiv',...
     'String','10',...
     'Units','normalized',...
     'Background','white',...
-    'Position',gridpos(layoutlines,layoutrows,1,1,1,1,0.05,0.025));
+    'Position',gridpos(layoutlines,layoutrows,1,2,3,4,0.01,0.01));
 
 uicontrol(Parent,'Style','Text',...
     'Tag','Text1',...
     'String','Number of divisions',...
     'Units','normalized',...
-    'Position',gridpos(layoutlines,layoutrows,2,2,1,1,0.01,0.03));
+    'Position',gridpos(layoutlines,layoutrows,1,2,1,2,0.01,0.01));
 
 e_percent=uicontrol(Parent,'Style','edit',...
     'Tag','edit_npoints',...
     'String','10',...
     'Units','normalized',...
     'Background','white',...
-    'Position',gridpos(layoutlines,layoutrows,1,1,2,2,0.05,0.025));
+    'Position',gridpos(layoutlines,layoutrows,1,2,7,8,0.01,0.01));
 
 uicontrol(Parent,'Style','Text',...
     'Tag','Text2',...
     'String','Evaluation points (%)',...
     'Units','normalized',...
-    'Position',gridpos(layoutlines,layoutrows,2,2,2,2,0.01,0.03));
+    'Position',gridpos(layoutlines,layoutrows,1,2,5,6,0.01,0.01));
          
 uicontrol(Parent,'style','pushbutton',...
           'string','Show',...
           'Callback',@show,...
           'Units','normalized',...
-          'Position',gridpos(layoutlines,layoutrows,1,2,4,4,0.05,0.05)); 
+          'Position',gridpos(layoutlines,layoutrows,1,2,9,20,0.01,0.01)); 
 
 % when OK is pushed uiwait ends 
 uicontrol(Parent,'style','pushbutton',...
           'string','OK',...
           'Callback','uiresume(gcbf)',...
           'Units','normalized',...
-          'Position',gridpos(layoutlines,layoutrows,1,2,5,5,0.05,0.05)); 
-
-
-% ===== GUI ELEMENTS TO CHANGE VIEW IN AXIS1 ===== %
-      
-% Toggle log scale for the data axes
-      
-uicontrol(Parent,'style','checkbox',...
-          'string','Log',...
-          'Callback',@togglelogscale,...
-          'Value', 0,...
-          'Units','normalized',...
-          'TooltipString','Toggle log scale',...
-          'Position',gridpos(66,30,13,16,1,2.2,0.01,0.005));
-      
-% Multiply y-axis by a factor of two
-      
-uicontrol(Parent,'style','pushbutton',...
-          'string','*2',...
-          'Callback',@doubleyscale,...
-          'Units','normalized',...
-          'TooltipString','Multiply axes by a factor of two',...
-          'Position',gridpos(66,30,55,58,1,2,0.01,0.005));
-      
-% Divide y-axis by a factor of two
-      
-uicontrol(Parent,'style','pushbutton',...
-          'string','/2',...
-          'Callback',@halfyscale,...
-          'Units','normalized',...
-          'TooltipString','Divide axes by a factor of two',...
-          'Position',gridpos(66,30,17,20,1,2,0.01,0.005));
-      
-% Autoscale y-axis
-      
-uicontrol(Parent,'style','pushbutton',...
-          'string','Y',...
-          'Callback',@autoyscale,...
-          'Units','normalized',...
-          'TooltipString','Autoscale axes',...
-          'Position',gridpos(66,30,21,54,1,2,0.01,0.005));
-      
-% Divide x-axis by a factor of two
-      
-uicontrol(Parent,'style','pushbutton',...
-          'string','/2',...
-          'Callback',@halfxscale,...
-          'Units','normalized',...
-          'TooltipString','Multiply axes by a factor of two',...
-          'Position',gridpos(66,30,13,16,28,29,0.01,0.005));
-      
-% Multiply x-axis by a factor of two
-      
-uicontrol(Parent,'style','pushbutton',...
-          'string','*2',...
-          'Callback',@doublexscale,...
-          'Units','normalized',...
-          'TooltipString','Divide axes by a factor of two',...
-          'Position',gridpos(66,30,13,16,3,4,0.01,0.005));
-      
-% slider x-axis
-      
-dataxslider = uicontrol(Parent,'style','slider',...
-          'string','/2',...
-          'Callback',@slidedataaxes,...
-          'Units','normalized',...
-          'TooltipString','Slide along the mass spec',...
-          'Position',gridpos(66,30,13,16,5,27,0.01,0.005));
+          'Position',gridpos(layoutlines,layoutrows,1,2,21,layoutrows,0.01,0.01)); 
       
 % Plot overview
       
@@ -184,7 +123,7 @@ uicontrol(Parent,'style','pushbutton',...
           'Callback',@plotoverview,...
           'Units','normalized',...
           'TooltipString','Plot whole mass spec (overview)',...
-          'Position',gridpos(66,30,59,62,1,2,0.01,0.005));
+          'Position',gridpos(layoutlines,layoutrows,29,30,1,2,0.01,0.01));
 
 % ############################## END OF LAYOUT
 
@@ -209,258 +148,117 @@ guidata(Parent,handles);
 
 show(Parent,0);
 
-    function show(hObject, ~)
-        handles=guidata(hObject);
-        handles.bgcorrectiondata.ndiv=str2double(get(e_ndiv,'String'));
-        handles.bgcorrectiondata.percent=str2double(get(e_percent,'String'));
-        %handles.bgcorrectiondata.polydegree=str2num(get(e_polydegree,'String'));
-        
-        % retrieve current view settings from axes:
-        if (handles.startup == 0) % only if we're not starting up any more...
-            xlim = get(axis1, 'XLim');
-            ylim = get(axis1, 'YLim');
-        end
-        
-        % read out the two fields for start and end mass. in case they are
-        % set to "start" and "end" respectively, set them to +- infinity
-        
-        % start mass
-        sm_str=get(e_startmass,'String');
-        if strcmp(sm_str,'start')
-            handles.bgcorrectiondata.startmass=-inf;
-        else
-            handles.bgcorrectiondata.startmass=str2double(sm_str);
-        end
-        
-        % end mass
-        em_str=get(e_endmass,'String');
-        if strcmp(em_str,'end')
-            handles.bgcorrectiondata.endmass=+inf;
-        else
-            handles.bgcorrectiondata.endmass=str2double(em_str);
-        end
+function show(hObject, ~)
+    handles=guidata(hObject);
+    handles.bgcorrectiondata.ndiv=str2double(get(e_ndiv,'String'));
+    handles.bgcorrectiondata.percent=str2double(get(e_percent,'String'));
+    %handles.bgcorrectiondata.polydegree=str2num(get(e_polydegree,'String'));
 
-        % call the function that actually calculates the background values
-        % for each section. it returns two lists: bgm und bgy - bgy gives a
-        % background level for each section, bgm the according mass (placed
-        % in the center of the section)
-        [handles.bgcorrectiondata.bgm,handles.bgcorrectiondata.bgy, handles.startind, handles.endind]=...
-            find_bg(handles.massaxis,handles.signal,...
-                handles.bgcorrectiondata.ndiv,...
-                handles.bgcorrectiondata.percent,...
-                handles.bgcorrectiondata.startmass,...
-                handles.bgcorrectiondata.endmass);
-        
-        % crop the mass and signal axes to the values used for determining
-        % the background levels
-        handles.massaxiscrop=handles.massaxis(handles.startind:handles.endind);
-        handles.signalcrop=handles.signal(handles.startind:handles.endind);
-        
-        % plot spectrum and fitted curve (pchip) between data points of background correction
-        plot(axis1,handles.massaxiscrop,handles.signalcrop,handles.massaxiscrop,interp1(handles.bgcorrectiondata.bgm,handles.bgcorrectiondata.bgy,handles.massaxiscrop,'pchip','extrap'));
-        
-        % reset zoom state to what it was before:
-        if (handles.startup == 0)
-            set(axis1, 'XLim', xlim)
-            set(axis1, 'YLim', ylim)
-        end
-        
-        % now we plotted something and it's definitely not startup conditions
-        % any more
-        handles.startup = 0;
-        
-        guidata(hObject,handles);
-        
+    % retrieve current view settings from axes:
+    if (handles.startup == 0) % only if we're not starting up any more...
+        xlim = get(axis1, 'XLim');
+        ylim = get(axis1, 'YLim');
     end
+
+    % read out the two fields for start and end mass. in case they are
+    % set to "start" and "end" respectively, set them to +- infinity
+
+    % start mass
+    sm_str=get(e_startmass,'String');
+    if strcmp(sm_str,'start')
+        handles.bgcorrectiondata.startmass=-inf;
+    else
+        handles.bgcorrectiondata.startmass=str2double(sm_str);
+    end
+
+    % end mass
+    em_str=get(e_endmass,'String');
+    if strcmp(em_str,'end')
+        handles.bgcorrectiondata.endmass=+inf;
+    else
+        handles.bgcorrectiondata.endmass=str2double(em_str);
+    end
+
+    % call the function that actually calculates the background values
+    % for each section. it returns two lists: bgm und bgy - bgy gives a
+    % background level for each section, bgm the according mass (placed
+    % in the center of the section)
+    [handles.bgcorrectiondata.bgm,handles.bgcorrectiondata.bgy, handles.startind, handles.endind]=...
+        find_bg(handles.massaxis,handles.signal,...
+            handles.bgcorrectiondata.ndiv,...
+            handles.bgcorrectiondata.percent,...
+            handles.bgcorrectiondata.startmass,...
+            handles.bgcorrectiondata.endmass);
+
+    % crop the mass and signal axes to the values used for determining
+    % the background levels
+    handles.massaxiscrop=handles.massaxis(handles.startind:handles.endind);
+    handles.signalcrop=handles.signal(handles.startind:handles.endind);
+
+    % plot spectrum and fitted curve (pchip) between data points of background correction
+    plot(axis1,handles.massaxiscrop,handles.signalcrop,handles.massaxiscrop,interp1(handles.bgcorrectiondata.bgm,handles.bgcorrectiondata.bgy,handles.massaxiscrop,'pchip','extrap'));
+
+    % reset zoom state to what it was before:
+    if (handles.startup == 0)
+        set(axis1, 'XLim', xlim)
+        set(axis1, 'YLim', ylim)
+    end
+
+    % now we plotted something and it's definitely not startup conditions
+    % any more
+    handles.startup = 0;
+
+    guidata(hObject,handles);
+
+end
 
 
 % ====FUNCTIONS NEEDED TO CHANGE VIEW IN AXIS1==== % 
 
-% initialize some status values needed for these functions 
+% initialize some status values needed for the view change functions
 handles.status.logscale = 0;
 handles.status.overview = 0;
 handles.status.lastlims = [[0 0] [0 0]];
 
-function togglelogscale(hObject, ~)
-        % This button toggles the logarithmic display of the data axes in
-        % y-direction.
-        
-        % get settings
-        handles = guidata(Parent);
-        
-        % turn warning about negative values off
-        warning('off', 'MATLAB:Axes:NegativeDataInLogAxis')
-        
-        % toggle function
-        if (get(hObject,'Value') == get(hObject,'Max'))
-            set(axis1, 'YScale', 'log');
-            handles.status.logscale = 1;
-        elseif (get(hObject,'Value') == get(hObject,'Min'))
-            set(axis1, 'YScale', 'linear');
-            handles.status.logscale = 0;
-        end
-        
-        % save back
-        guidata(Parent,handles);
+function plotoverview(hObject, ~)
+    % get settings
+    handles = guidata(Parent);
+
+    % if the user jumped away from an overview, we don't want to jump
+    % back to the old coordinates
+
+    % crude hack: if the viewed range is much (2x) smaller than the
+    % full mass range we were probably not in overview mode. if at the
+    % same time overview is still true, the user probably jumped out of
+    % overview mode to a molecule and we should now go to overview
+    cl = get(axis1, 'XLim');
+    viewedrange = (cl(2) - cl(1))*2;
+
+    maxmass = max(handles.massaxiscrop);
+
+    if (viewedrange <= maxmass && handles.status.overview == 1)
+        handles.status.overview = 0;
     end
 
-
-    function doubleyscale(hObject, ~)
-        % This function multiplies the Y-axis with a factor of two (hence
-        % making the signals smaller)
-        
-        % current limits
-        cl = get(axis1, 'YLim');
-        % multiply
-        nl = [cl(1)*2 cl(2)*2];
-        % set back
-        set(axis1, 'YLim', nl)
-    end
-
-    function doublexscale(hObject, ~)
-        % This function multiplies the Y-axis with a factor of two (hence
-        % making the signals smaller)
-        
-        % current limits
-        cl = get(axis1, 'XLim');
-        %half width to add
-        hw = (cl(2)-cl(1))/2;
-        % add
-        nl = [cl(1)-hw cl(2)+hw];
-        % set back
-        set(axis1, 'XLim', nl);
-        
-        updateslider;
-    end
-
-    function halfyscale(hObject, ~)
-        % This function divides the Y-axis by a factor of two (hence
-        % making the signals bigger)
-        
-        % current limits
-        cl = get(axis1, 'YLim');
-        % divide
-        nl = [cl(1)/2 cl(2)/2];
-        % set back
-        set(axis1, 'YLim', nl)
-    end
-
-    function halfxscale(hObject, ~)
-        % This function divides the Y-axis by a factor of two (hence
-        % making the signals bigger)
-        
-        % current limits
-        cl = get(axis1, 'XLim');
-        % quarter width to add
-        hw = (cl(2)-cl(1))/4;
-        % add
-        nl = [cl(1)+hw cl(2)-hw];
-        % set back
-        set(axis1, 'XLim', nl);
-        
-        updateslider;
-    end
-
-    function autoyscale(hObject, ~)
-        % This function divides the Y-axis by a factor of two (hence
-        % making the signals bigger)
-        
-        % set back
+    % are we already in overview?
+    if handles.status.overview == 0
+        % save the old settings so we can toggle back
+        oxl = get(axis1, 'XLim');
+        oyl = get(axis1, 'YLim');
+        handles.status.lastlims = [oxl oyl];
         set(axis1, 'YLimMode', 'auto');
+        set(axis1, 'XLimMode', 'auto');
+        handles.status.overview = 1;
+    elseif handles.status.overview == 1
+        % jump back to last settings
+        set(axis1, 'XLim', [handles.status.lastlims(1) handles.status.lastlims(2)]);
+        set(axis1, 'YLim', [handles.status.lastlims(3) handles.status.lastlims(4)]);
+        handles.status.overview = 0;
     end
 
-    function updateslider(hObject, ~)
-        % This function updates the x-axis slider accordingly whenever 
-        % something changes in the dataaxes
-        
-        % supress warnings in case the user scrolls and pans around in an
-        % uncontrolled manner and withour fear of disaster:
-        warning('off', 'MATLAB:hg:uicontrol:ParameterValuesMustBeValid');
-        
-        % get settings
-        handles = guidata(Parent);
-        
-        % calculate where around we are centered
-        xlims = get(axis1, 'XLim');
-        com = (xlims(2) + xlims(1))/2;
-        viewedrange = xlims(2) - xlims(1);
-        
-        % how big is our massspec?
-        try
-            maxmass = max(handles.massaxiscrop);
-        catch
-            maxmass = 1;
-        end
-        
-        % we should calculate the width of our slider
-        slwidth = viewedrange/maxmass;
-        % set the max to the maximum mass
-        set(dataxslider, 'Max', maxmass);
-        % since the Max is our mass range we can set this to the center of
-        % mass
-        set(dataxslider, 'Value', com);
-        % set the slider width
-        set(dataxslider, 'SliderStep', [slwidth/10 slwidth])
-    end
-
-    function slidedataaxes(hObject, ~)
-        % This function updates the data axes when the slider for the
-        % x-axis is clicked
-        
-        % calculate where around we are centered
-        cl = get(axis1, 'XLim');
-        % we need to add / substract half of the currently viewed range to
-        % the new center of mass
-        vrhalf = (cl(2) - cl(1))/2;
-        
-        % get center of mass
-        com = get(dataxslider, 'Value');
-        
-        % new viewing range
-        nl = [com-vrhalf com+vrhalf];
-        % jump by one view range
-        set(axis1, 'XLim', nl);
-    end
-
-    function plotoverview(hObject, ~)
-        % get settings
-        handles = guidata(Parent);
-        
-        % if the user jumped away from an overview, we don't want to jump
-        % back to the old coordinates
-        
-        % crude hack: if the viewed range is much (2x) smaller than the
-        % full mass range we were probably not in overview mode. if at the
-        % same time overview is still true, the user probably jumped out of
-        % overview mode to a molecule and we should now go to overview
-        cl = get(axis1, 'XLim');
-        viewedrange = (cl(2) - cl(1))*2;
-        
-        maxmass = max(handles.massaxiscrop);
-        
-        if (viewedrange <= maxmass && handles.status.overview == 1)
-            handles.status.overview = 0;
-        end
-        
-        % are we already in overview?
-        if handles.status.overview == 0
-            % save the old settings so we can toggle back
-            oxl = get(axis1, 'XLim');
-            oyl = get(axis1, 'YLim');
-            handles.status.lastlims = [oxl oyl];
-            set(axis1, 'YLimMode', 'auto');
-            set(axis1, 'XLimMode', 'auto');
-            handles.status.overview = 1;
-        elseif handles.status.overview == 1
-            % jump back to last settings
-            set(axis1, 'XLim', [handles.status.lastlims(1) handles.status.lastlims(2)]);
-            set(axis1, 'YLim', [handles.status.lastlims(3) handles.status.lastlims(4)]);
-            handles.status.overview = 0;
-        end
-        
-        % save back
-        guidata(Parent,handles);
-    end
+    % save back
+    guidata(Parent,handles);
+end
 
 uiwait(Parent)
 
