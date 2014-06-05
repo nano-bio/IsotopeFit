a9440b71c6b4850a306de567afd1b66f182eb725
matzeharnisch <matzeharnisch@yahoo.de>
2014-06-05 14:15:30 +0200

    bugfix for displaying peaks in lower massrange, changed mass2ind.m (done by ralsi)

--- mass2ind.m
+++ mass2ind.m
@@ -2,12 +2,7 @@ function out = mass2ind(massaxis,mass)
 %out = mass2ind(massaxis,mass)
 %finds nearest mass in massaxis and returns index
 
-if massaxis(end)>=mass;
-    out=find(massaxis>=mass,1);
-else
-    out=length(massaxis);
-end
-
+[~,out]=min(abs(massaxis-mass));
 
 end
 
