91c8eed490c2b21e8058513c121c93d307ce833e
gatchell <gatchell@fysik.su.se>
2020-05-18 13:27:18 +0200

    Compatability cleanup

--- h5cal.m
+++ h5cal.m
@@ -15,22 +15,30 @@ function ma = h5cal(datafile)
         % assume they are real
         eval(['assume(' pnames{i} ', ''real'')'])
     end
-    syms i m;
+    syms symi m;
 
     % assume indices and masses to be larger than zero
     assume(m > 0);
-    assume(i > 0);
+    assume(symi > 0);
     assume(m, 'real');
-    assume(i, 'integer');
+    assume(symi, 'integer');
 
     % FUN FACT: the last character that comes out of the h5 file looks like a
     % space (ASCII 32), but it isn't! It's a NUL instead (ASCII 0). Solve
     % doesn't like that kind of trickery, so we have to remove it (strtrim
     % doesn't work - doesn't trim ASCII 0).
-    mcf = mcf(1:end-1);
+    %mcf = mcf(1:end-1);
+    mcf = strcat('symi',mcf(2:end-1));
 
     % solve for m; we only want real solutions
-    reverse_mcf = solve(mcf, m, 'Real', true);
+    if ~verLessThan('matlab', '9.3') % for matlab 2017b and newer
+        warning('off','symbolic:solve:SolutionsDependOnConditions')
+        reverse_mcf = solve(str2sym(mcf), m, 'real', true,'ReturnConditions',false);
+        warning('on','symbolic:solve:SolutionsDependOnConditions')
+    else
+        reverse_mcf = solve(mcf, m, 'real', true);
+    end
+    %a=0.0;
 
     % assign numerical values to the parameters (symbols not needed any more)
     for n = 1:length(pnames)
@@ -39,7 +47,7 @@ function ma = h5cal(datafile)
     end
     
     % create linspace of indices
-    i = 1:double(samples);
+    symi = 1:double(samples);
     % evaluate the reversed calibration function to retrieve massaxis
     ma = eval(reverse_mcf);
     % apparantely in some cases this functions evaluates to some complex
